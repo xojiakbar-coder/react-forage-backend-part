2cbe56e62e4202b4c859db83476d5ecc1bfb03e4
Xojiakbar <xojiakbarisroilov.777@gmail.com>
2025-09-08 00:00:27 +0500

    vercel configuration

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://react-forage-backend-inimitable.vercel.app",
       },
     ],
   },
